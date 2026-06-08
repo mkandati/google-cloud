13d5c52ec647818ba39af6efdf6c4cdf9aeea612
mkandati <mukandat456@gmail.com>
2026-06-09 01:58:37 +0530

    updated main.tf file

--- main.tf
+++ main.tf
@@ -6,4 +6,6 @@ provider "google" {
 resource "google_storage_bucket" "demo" {
   name     = "mkandati-demo-bucket-123456"
   location = "US"
+  force_destroy = true
+  uniform_bucket_level_access = true
 }
