479d09c05a2057bb4cfd17ead33ba51963e25ceb
mkandati <mukandat456@gmail.com>
2026-06-09 01:37:37 +0530

    terraform and main.tf file updated

--- google-cloud/terraform/main.tf
+++ google-cloud/terraform/main.tf
@@ -1,12 +1,11 @@
 provider "google" {
-  project     = "my-project-id"
-  region      = "us-central1"
+  project = "terraform-gcp-498813"
+  region  = "us-central1"
 }
 
-resource = "google_storage_bucket" "my-test-bucket" {
-    name        = "demo-bucket-1357"
-    localtion   = "us-central1"
-    project     = "terraform-gcp-498813"
-    force_destroy   = true
-    policy_access_prevention    = "enforced"   
+resource "google_storage_bucket" "bucket" {
+  name     = "mkandati-terraform-demo-12345"
+  location = "US"
+
+  force_destroy = true
 }
