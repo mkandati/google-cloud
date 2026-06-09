35a8ad95d90d2f6f5586b5095f702ac76032a178
mkandati <mukandat456@gmail.com>
2026-06-09 11:26:45 +0530

    updated main.tf with gce info

--- main.tf
+++ main.tf
@@ -3,9 +3,24 @@ provider "google" {
   region  = "us-central1"
 }
 
-resource "google_storage_bucket" "demo" {
+/*resource "google_storage_bucket" "demo" {
   name     = "mkandati-demo-bucket-123456"
   location = "US"
   force_destroy = true
   uniform_bucket_level_access = true
+}*/
+
+resource "google_compute_instance" "default" {
+    name    = "my-instance"
+    machine_type    = "e2-micro"
+    zone            = "us-central1-a"
+
+    boot_disk {
+        initialize_params {
+            image   = "Ubuntu 26.04 LTS Minimal"
+            labels  = {
+                my_label    = "value"
+            }
+        }
+    }
 }
